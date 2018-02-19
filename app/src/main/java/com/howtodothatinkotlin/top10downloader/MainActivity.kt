6721cdcc65442d8bf7f9ee7ff8593cce43e2852f
Katelyn Rodrigue <krodrigu@tulane.edu>
2018-02-19 09:10:40 -0500

    make method to read the XML

--- app/src/main/java/com/howtodothatinkotlin/top10downloader/MainActivity.kt
+++ app/src/main/java/com/howtodothatinkotlin/top10downloader/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.howtodothatinkotlin.top10downloader
 
-import android.content.Context
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG = "Main Activity"
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,26 +20,69 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "onCreate called")
         val downloadData = DownloadData()
-        downloadData.execute("URL goes here")
-        Log.d(TAG, "onCreate done")
-
-
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        Log.d(TAG, "onCreate: done")
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>(){
-            private val TAG = "Download Data"
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
 
             override fun onPostExecute(result: String?) {
                 super.onPostExecute(result)
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg p0: String?): String {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                Log.d(TAG, "doInBackround: starts with ${p0[0]}")
-                return "doInBackground completed"
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+                val xmlResult = StringBuilder()
+
+                try {
+                    val url = URL(urlPath)
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val response = connection.responseCode
+                    Log.d(TAG, "downloadXML: The response code was $response")
+
+//            val inputStream = connection.inputStream
+//            val inputStreamReader = InputStreamReader(inputStream)
+//            val reader = BufferedReader(inputStreamReader)
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+
+                    val inputBuffer = CharArray(500)
+                    var charsRead = 0
+                    while (charsRead >= 0) {
+                        charsRead = reader.read(inputBuffer)
+                        if (charsRead > 0) {
+                            xmlResult.append(String(inputBuffer, 0, charsRead))
+                        }
+                    }
+                    reader.close()
+
+                    Log.d(TAG, "Received ${xmlResult.length} bytes")
+                    return xmlResult.toString()
+
+                } catch (e: MalformedURLException) {
+                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+                } catch (e: IOException) {
+                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+//                } catch (e: Exception) {
+//                    Log.e(TAG, "Unknown error: ${e.message}")
+                }
+
+                return ""  // If it gets to here, there's been a problem. Return an empty string
             }
         }
     }
+
+
 }
+
+
